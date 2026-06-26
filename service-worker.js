0050af001dc38e0c807f80c0f56140810057d6aa
Abraham Kapambwe <abrahamkapambwe@gmail.com>
2026-06-26 20:41:39 +0200

    deploy: FieldOpsApp @ 2026-06-26 20:41 UTC

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 2k6mMh5y */
+/* Manifest version: 9IIauZ1F */
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
